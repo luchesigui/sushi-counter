3010484313178cd2d9103b05a5f78b258d3c5407
dedekpo <dedekpo@gmail.com>
2024-09-29 20:55:05 -0300

    make price 9.99

--- app/api/create-checkout-mp/route.ts
+++ app/api/create-checkout-mp/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: NextRequest) {
             title: "Página Loveyuu",
             picture_url: "https://www.loveyuu.com/favicon.ico",
             quantity: 1,
-            unit_price: 3.99,
+            unit_price: 9.99,
           },
         ],
         payment_methods: {
