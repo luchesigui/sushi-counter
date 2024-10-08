abf9bfb4c63c084a71c6233e31b5999a84e5dd8b
dedekpo <dedekpo@gmail.com>
2024-10-07 22:57:04 -0300

    added payer information

--- app/api/create-checkout-mp/route.ts
+++ app/api/create-checkout-mp/route.ts
@@ -11,6 +11,24 @@ export async function POST(req: NextRequest) {
         metadata: {
           test: "123",
         },
+        payer: {
+          name: "André",
+          surname: "Elias",
+          email: "dedekpo@gmail.com",
+          phone: {
+            area_code: "31",
+            number: "983399692",
+          },
+          identification: {
+            type: "CPF",
+            number: "13761278659",
+          },
+          address: {
+            street_name: "Rua Cristália",
+            street_number: "148",
+            zip_code: "32684036",
+          },
+        },
         items: [
           {
             id: "pagina-loveyuu-19",
